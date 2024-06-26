7f171ada7a06b12ce6f126065bb510c37ddc147c
fcesc-code <bpfran@gmail.com>
2024-06-26 23:24:58 +0200

    feature / add a basic question component with a title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuestionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'trivia';
+  tags = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    {
+      title: 'Angular Language Service',
+      link: 'https://angular.dev/tools/language-service',
+    },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
 }
